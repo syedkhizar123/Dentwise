28726b7547ce65d46d264da33c586407d45c5291
syedkhizar123 <khizarrehman2007@gmail.com>
2026-04-01 23:31:44 +0500

    feat: refactor Home component for user authentication and navigation; update dependencies and add new header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,19 +29,6 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ReactQueryProvider>
           <ClerkProvider>
-            <header className="flex justify-end items-center p-4 gap-4 h-16">
-              <Show when="signed-out">
-                <SignInButton mode='modal' />
-                <SignUpButton mode='modal'>
-                  <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </Show>
-              <Show when="signed-in">
-                <UserButton />
-              </Show>
-            </header>
             {children}
           </ClerkProvider>
         </ReactQueryProvider>
